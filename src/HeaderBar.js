ced0c35a8e6220953e1b18102919f6982a46c13c
jmk5315 <31719854+jmk5315@users.noreply.github.com>
2022-10-04 16:49:01 +0900

    상단바 고정

---
+++ src/HeaderBar.js
@@ -0,0 +1,19 @@
+import React from "react";
+
+const HeaderBar = () => {
+  return (
+    <div style={{ height: "180px", width: "100%", backgroundColor: "red" }}>
+      <center>
+        <span
+          style={{
+            fontSize: 60,
+          }}
+        >
+          EricAuction
+        </span>
+      </center>
+    </div>
+  );
+};
+
+export default HeaderBar;
